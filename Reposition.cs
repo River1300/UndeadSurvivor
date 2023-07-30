938bae08c4fc15cad0c1fff535e021106be9f26c
River <hom0906@naver.com>
2023-07-30 22:26:09 +0900

    Map

---
+++ Reposition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(!other.CompareTag("Area")) return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        switch(transform.tag)
+        {
+            case "Ground":
+                float diffX = playerPos.x - myPos.x;
+                float diffY = playerPos.y - myPos.y;
+                float dirX = diffX < 0 ? -1 : 1;
+                float dirY = diffY < 0 ? -1 : 1;
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+                transform.Translate(diffX < diffY ? 
+                    Vector3.up * dirY * 60 :
+                    Vector3.right * dirX * 60
+                    );
+                break;
+
+            case "Enemy":
+                break;
+        }
+    }
+}
